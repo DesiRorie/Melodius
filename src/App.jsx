49162d5b9a18427444a88e82eacd6ff0680f8846
DesiRorie <desirorie@icloud.com>
2023-04-12 21:49:09 -0400

    ".."

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Melodius/test" element={<Test />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
 
         <BottomNav />
