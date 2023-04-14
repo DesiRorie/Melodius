e09663259a66baf095adb050b0b5ef7cc4a5ba85
DesiRorie <desirorie@icloud.com>
2023-04-13 23:05:00 -0400

    "styles"

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Test = () => {
-  return <div></div>;
+  const isMobile = useMediaQuery("(max-width: 600px)");
+  return (
+    // <div className="searchDiv">
+    //   <ArrowBackIosNewIcon />
+    //   <ArrowForwardIosIcon />
+    //   <input
+    //     className="searchInput"
+    //     placeholder="What do you want to listen to "
+    //   />
+    // </div>
+    <div className="searchDiv">
+      {isMobile && <h2>Search</h2>}
+      {!isMobile && <ArrowBackIosNewIcon />}
+      {!isMobile && <ArrowForwardIosIcon />}
+      <input
+        className="searchInput"
+        placeholder="What do you want to listen to?"
+      />
+    </div>
+  );
 };
 
 export default Test;
