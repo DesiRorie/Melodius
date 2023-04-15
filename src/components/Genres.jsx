68368c0e70e0ac1dd4c03539ef033503eec0da6d
DesiRorie <desirorie@icloud.com>
2023-04-15 01:55:20 -0400

    "styles"

---
+++ src/components/Genres.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const Genres = () => {
+  // const color = #FFFFFF - White
+  // #FFD700 - Gold
+  // #FFA07A - Light Salmon
+  // #FF6347 - Tomato
+  // #FF4500 - Orange Red
+  // #FF69B4 - Hot Pink
+  // #FFC0CB - Pink
+  // #FF00FF - Fuchsia
+  // #00FFFF - Cyan
+  // #00FF00 - Lime
+  // #7FFF00 - Chartreuse
+  // #32CD32 - Lime Green
+  // #00CED1 - Dark Turquoise
+  // #87CEEB - Sky Blue
+  // #4169E1 - Royal Blue
+  // #6A5ACD - Slate Blue
+  // #9400D3 - Dark Violet
+  // #8B008B - Dark Magenta
+  // #FF1493 - Deep Pink
+  // #FFB6C1 - Light Pink
+  return (
+    <div className="genreBox">
+      <div style={{ backgroundColor: "" }} className="genres">
+        <h3>Podcasts</h3>
+      </div>
+      <div style={{ backgroundColor: "#FFA07A" }} className="genres">
+        <h3>Audiobooks</h3>
+      </div>
+      <div style={{ backgroundColor: "#FF6347" }} className="genres">
+        <h3>Live Events</h3>
+      </div>
+      <div style={{ backgroundColor: "#FF4500" }} className="genres">
+        <h3>Made For You</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FF69B4" }} className="genres">
+        <h3>New Releases</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FFC0CB" }} className="genres">
+        <h3>Hip-Hop</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FF00FF" }} className="genres">
+        <h3>Pop</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#00FFFF" }} className="genres">
+        <h3>Made For You</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "brown" }} className="genres">
+        <h3>Country</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "green" }} className="genres">
+        <h3>Latin</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "blue" }} className="genres">
+        <h3>Charts</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#00CED1" }} className="genres">
+        <h3>Rock</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#87CEEB" }} className="genres">
+        <h3>Dance/Electronic</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#6A5ACD" }} className="genres">
+        <h3>Mood</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#9400D3" }} className="genres">
+        <h3>Indie</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FF1493" }} className="genres">
+        <h3>Discover</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FF1493" }} className="genres">
+        <h3>Radio</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FF1493" }} className="genres">
+        <h3>Disney</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "#FF1493" }} className="genres">
+        <h3>R&B</h3>
+      </div>{" "}
+      <div style={{ backgroundColor: "blue" }} className="genres">
+        <h3>Christian Gospel</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Genres;
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,27 +2,23 @@ import React from "react";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Genres from "./Genres";
 
 const Test = () => {
   const isMobile = useMediaQuery("(max-width: 600px)");
   return (
-    // <div className="searchDiv">
-    //   <ArrowBackIosNewIcon />
-    //   <ArrowForwardIosIcon />
-    //   <input
-    //     className="searchInput"
-    //     placeholder="What do you want to listen to "
-    //   />
-    // </div>
-    <div className="searchDiv">
-      {isMobile && <h2>Search</h2>}
-      {!isMobile && <ArrowBackIosNewIcon />}
-      {!isMobile && <ArrowForwardIosIcon />}
-      <input
-        className="searchInput"
-        placeholder="What do you want to listen to?"
-      />
-    </div>
+    <>
+      <div className="searchDiv">
+        {isMobile && <h2>Search</h2>}
+        {!isMobile && <ArrowBackIosNewIcon />}
+        {!isMobile && <ArrowForwardIosIcon />}
+        <input
+          className="searchInput"
+          placeholder="What do you want to listen to?"
+        />
+      </div>{" "}
+      <Genres />
+    </>
   );
 };
 
